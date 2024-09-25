aec8bdd38b06361c2379c22c84d05ec159701f72
dangdz199 <dangpham0392@gmail.com>
2024-09-25 14:27:12 +0700

    remove ##

--- laptop_shop/urls.py
+++ laptop_shop/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('laptops/', laptop_list, name='laptop_list'),  # URL for laptop listing
     path('laptops/<int:pk>/', laptop_detail, name='laptop_detail'),  # URL for laptop detail
 ]
-#cmn
+
 # Serve media files in development mode
 from django.conf import settings
 from django.conf.urls.static import static
