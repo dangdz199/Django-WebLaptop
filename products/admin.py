1c1ef16b8f4ddeea5ed9b36a970b16096a68f67f
huythong <huythong0932@gmail.com>
2024-09-26 08:33:04 +0700

    Test4

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Laptop
+from .models import Laptop, ProductImages
 
-admin.site.register(Laptop)
- #concac
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImages
+    extra = 1
+
+@admin.register(Laptop)
+class LaptopAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesInline]
+
+admin.site.register(ProductImages)
