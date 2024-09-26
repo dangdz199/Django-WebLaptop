88bfdf061fa37796f616de61638ff3f19e869496
huythong <huythong0932@gmail.com>
2024-09-26 08:43:29 +0700

    Test7

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from .models import Laptop, ProductImages
 
-# Tạo Inline cho ProductImages để hiển thị hình ảnh phụ trong trang Laptop
 class ProductImagesInline(admin.TabularInline):
     model = ProductImages
-    extra = 3 # Số lượng form trống để thêm hình ảnh mới
+    extra = 1
 
-# Tùy chỉnh hiển thị model Laptop
+@admin.register(Laptop)
 class LaptopAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'created_at')  # Hiển thị các cột trong danh sách
-    inlines = [ProductImagesInline]  # Đưa ProductImages vào trang chi tiết laptop
+    inlines = [ProductImagesInline]
 
-# Đăng ký các model với admin
-admin.site.register(Laptop, LaptopAdmin)
 admin.site.register(ProductImages)
